a4f23c808d9962ddac6447669bc8b55748873b69
shellphy <ai.shellphy@gmail.com>
2025-08-07 22:58:10 +0800

    fix

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import os
 
 def main():
-    print("Hello from test-f5!")
-    
-    print("111")
-  
     os.environ['HF_HOME'] = "./cache/"
-    os.environ['TRANSFORMERS_CACHE'] = "./cache/"
     from f5_tts.api import F5TTS
-    
-    print("222")
-    
     f5tts = F5TTS()
+    
     f5tts.infer(
         ref_file="./basic_ref_zh.wav",
         ref_text="对，这就是我，万人敬仰的太乙真人。",
